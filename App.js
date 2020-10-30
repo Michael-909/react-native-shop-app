756d1e8a392b29b295e2451438701f05b21860a2
Thomas Wingo <santa960727@gmail.com>
2020-10-30 15:34:45 -0400

    redux setup

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { combineReducers, createStore } from 'redux';
+
+import productReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productReducer
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+    return <FlatList />;
+};
+
+const styles = StyleSheet.create({
+});
+
+export default ProductsOverviewScreen;
