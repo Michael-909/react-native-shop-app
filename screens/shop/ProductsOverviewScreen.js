c96cb084dec3c1ff7b8b158baf7d81668137ac96
Thomas Wingo <santa960727@gmail.com>
2020-11-01 13:37:34 -0500

    adding pull to refresh

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -11,19 +11,20 @@ import { fetchProducts } from '../../store/actions/products';
 
 const ProductsOverviewScreen = props => {
 	const [isLoading, setIsLoading] = useState(false);
+	const [isRefreshing, setIsRefreshing] = useState(false);
 	const [error, setError] = useState();
 	const products = useSelector(state => state.products.availableProducts);
 	const dispatch = useDispatch();
 
 	const loadProducts = useCallback(async () => {
 		setError(null);
-		setIsLoading(true);
+		setIsRefreshing(true);
 		try {
 			await dispatch(fetchProducts());
 		} catch(err) {
 			setError(err.message);
 		}
-		setIsLoading(false);
+		setIsRefreshing(false);
 	}, [dispatch, setIsLoading, setError]);
 
 	useEffect(() => {
@@ -34,7 +35,8 @@ const ProductsOverviewScreen = props => {
 	}, [loadProducts]);
 
 	useEffect(() => {
-		loadProducts();
+		setIsLoading(true);
+		loadProducts().then(() => {setIsLoading(false);});
 	}, [dispatch, loadProducts]);
 
 	const selectItemHandler = (id, title) => {
@@ -63,6 +65,7 @@ const ProductsOverviewScreen = props => {
 
 	return (
 		<FlatList data={products}
+			onRefresh={loadProducts} refreshing={isRefreshing}
 			keyExtractor={item => item.id}
 			renderItem={itemData => 
 				<ProductItem 
--- store/actions/orders.js
+++ store/actions/orders.js
@@ -4,38 +4,38 @@ export const ADD_ORDER = 'ADD_ORDER';
 export const SET_ORDERS = 'SET_ORDERS';
 
 export const fetchOrders = () => {
-    return async dispatch => {
-        const response = await fetch('https://react-native-shop-app-850ef.firebaseio.com/orders/u1.json');
+	return async dispatch => {
+		const response = await fetch('https://react-native-shop-app-850ef.firebaseio.com/orders/u1.json');
 		if(!response.ok) {
 			throw new Error('Something went wrong!');
 		}
-        const result = await response.json();
-        const loadedOrders = [];
-        for(const key in result) {
-            loadedOrders.push(new Order(
-                key, result[key].cartItems, result[key].totalAmount, new Date(result[key].date)
-            ));
-        }
+		const result = await response.json();
+		const loadedOrders = [];
+		for(const key in result) {
+			loadedOrders.push(new Order(
+				key, result[key].cartItems, result[key].totalAmount, new Date(result[key].date)
+			));
+		}
 
-        dispatch({
-            type: SET_ORDERS,
-            orders: loadedOrders
-        });
-    };
+		dispatch({
+			type: SET_ORDERS,
+			orders: loadedOrders
+		});
+	};
 };
 
 export const addOrder = (cartItems, totalAmount) => {
-    return async dispatch => {
-        const date = new Date();
-        const response = await fetch('https://react-native-shop-app-850ef.firebaseio.com/orders/u1.json', {
+	return async dispatch => {
+		const date = new Date();
+		const response = await fetch('https://react-native-shop-app-850ef.firebaseio.com/orders/u1.json', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-                cartItems,
-                totalAmount,
-                date: date.toISOString()
+				cartItems,
+				totalAmount,
+				date: date.toISOString()
 			})
 		});
 		if(!response.ok) {
@@ -43,14 +43,14 @@ export const addOrder = (cartItems, totalAmount) => {
 		}
 		const result = await response.json();
 
-        dispatch({
-            type: ADD_ORDER,
-            orderData: {
-                id: result.name,
-                items: cartItems,
-                amount: totalAmount,
-                date
-            }
-        });
-    };
+		dispatch({
+			type: ADD_ORDER,
+			orderData: {
+				id: result.name,
+				items: cartItems,
+				amount: totalAmount,
+				date
+			}
+		});
+	};
 };
